07a54f2e216851b2046b828ca0cc79f08f1ebdba
user <user>
2019-02-07 08:59:19 +0000

    Init!

---
+++ mycolors.py
@@ -0,0 +1,243 @@
+#from math import *
+#from turtle import *
+
+import numpy as np
+from numpy import sin, cos, arctan2 as atan2, \
+                  sqrt, ceil, floor, degrees, radians, log, pi, exp, transpose
+from colorio import CIELAB, CAM16UCS, JzAzBz, SrgbLinear
+
+L_A = 64 / pi / 5
+srgb = SrgbLinear()
+lab = CIELAB()
+cam16ucs = CAM16UCS(0.69, 20, L_A)
+jzazbz = JzAzBz()
+
+NEUTRAL = '#262626'
+
+def lab_to_srgb(color):
+    return transpose(srgb.to_srgb1(srgb.from_xyz100(lab.to_xyz100(transpose(color)))))
+
+def jzazbz_to_srgb(color):
+    return transpose(srgb.to_srgb1(srgb.from_xyz100(jzazbz.to_xyz100(transpose(color)))))
+
+def srgb_to_lab(color):
+    return transpose(lab.from_xyz100(srgb.to_xyz100(srgb.from_srgb1(transpose(color)))))
+
+def srgb_to_jzazbz(color):
+    return transpose(jzazbz.from_xyz100(srgb.to_xyz100(srgb.from_srgb1(transpose(color)))))
+
+def lab_to_jzazbz(color):
+    return transpose(jzazbz.from_xyz100(lab.to_xyz100(transpose(color))))
+
+def lab_to_cam16ucs(color):
+    return transpose(cam16ucs.from_xyz100(lab.to_xyz100(transpose(color))))
+
+def jzazbz_to_lab(color):
+    return transpose(lab.from_xyz100(jzazbz.to_xyz100(transpose(color))))
+
+def cam16ucs_to_srgb(color):
+    return transpose(srgb.to_srgb1(srgb.from_xyz100(cam16ucs.to_xyz100(transpose(color)))))
+
+def srgb_to_cam16ucs(color):
+    return transpose(cam16ucs.from_xyz100(srgb.to_xyz100(srgb.from_srgb1(transpose(color)))))
+
+def _square_plot(l, k1, k2, conv, d=30):
+    for x in range(-300,301,d):
+        for y in range(-300,301,d):
+            goto(x, y)
+            c = conv(l, x * k1, y * k2)
+            if np.max(c) > 1.0 or np.min(c) < 0.0 or np.isnan(c).any():
+                tc = NEUTRAL
+            else:
+                tc = tuple(map(float, c))
+            dot(d, tc)
+    update()
+
+def plot_lab(l=73, r=30):
+    _square_plot(l, 1/4, 1/4, lab_to_srgb, r)
+
+def plot_cam16ucs(j=75, r=30):
+    _square_plot(j, 1/10, 1/10, cam16ucs_to_srgb, r)
+
+def plot_jzazbz(jz=0.12, r=30):
+    _square_plot(jz, 1/3000, 1/3000, jzazbz_to_srgb, r)
+
+def _diag_plot(l, k1, k2, conv, d=30):
+    x_step = int(round(tan(pi/3) * d/2))+1
+    for x in range(-300,301,x_step):
+        offset = (x//x_step) % 2 * d//2
+        for y in range(-300+offset, 301+offset,d+1):
+            goto(x, y)
+            c = conv(l, x * k1, y * k2)
+            if np.max(c) > 1.0 or np.min(c) < 0.0 or np.isnan(c).any():
+                tc = NEUTRAL
+            else:
+                tc = tuple(map(float, c))
+            dot(d, tc)
+    update()
+
+def dplot_lab(l=73, d=30):
+    _diag_plot(l, 1/4, 1/4, lab_to_srgb, d)
+
+def dplot_cam16ucs(j=75, d=30):
+    _diag_plot(j, 1/10, 1/10, cam16ucs_to_srgb, d)
+
+def dplot_jzazbz(jz=0.12, d=30):
+    _diag_plot(jz, 1/3000, 1/3000, jzazbz_to_srgb, d)
+
+def _radial_plot(l, k1, k2, conv, big=True):
+    for i in range(1,40,3 if big else 2):
+        r = 10 * i
+        points = i*(2 if big else 3)
+        for j in range(points):
+            angle = 2*pi * j/points
+            x = r*cos(angle)
+            y = r*sin(angle)
+            goto(x, y)
+            c = conv(l, x * k1, y * k2)
+            if np.max(c) > 1.0 or np.min(c) < 0.0 or np.isnan(c).any():
+                tc = NEUTRAL
+            else:
+                tc = tuple(map(float, c))
+            dot(29 if big else 19, tc)
+    update()
+
+def rplot_lab(l=73, big=True):
+    _radial_plot(l, 1/4, 1/4, lab_to_srgb, big)
+
+def rplot_jzazbz(jz=0.12, big=True):
+    _radial_plot(jz, 1/3000, 1/3000, jzazbz_to_srgb, big)
+
+def rplot_cam16ucs(j=75, big=True):
+    _radial_plot(j, 1/10, 1/10, cam16ucs_to_srgb, big)
+
+def to_radians(a):
+    return a / 180 * pi
+
+def bright_jzazbz(jz, angle, steps=100):
+    prev_c = None
+    r = 0
+    for i in range(50000):
+        r += 0.1 / steps
+        x = r*cos(to_radians(angle))
+        y = r*sin(to_radians(angle))
+        c = jzazbz_to_srgb((jz, x, y))
+        if np.max(c) > 1.0 or np.min(c) < 0.0 or np.isnan(c).any():
+            print(i)
+            return prev_c
+        else:
+            prev_c = c
+
+def bright_cam16ucs(j, angle, steps=100):
+    prev_c = None
+    r = 0
+    for i in range(50000):
+        r += 30 / steps
+        a = r*cos(to_radians(angle))
+        b = r*sin(to_radians(angle))
+        c = cam16ucs_to_srgb((j, a, b))
+        if np.max(c) > 1.0 or np.min(c) < 0.0 or np.isnan(c).any():
+            #print(i)
+            return prev_c
+        else:
+            prev_c = c
+
+# !!!
+def show_jzazbz_hues():
+    for j in range(0,11):
+        steps = 32
+        if j == 0: steps = 4
+        if j == 1: steps = 8
+        if j == 2: steps = 16
+        for i in range(steps):
+            r = 20 + 50*j
+            angle = 360 * i/steps
+            goto(r*cos(to_radians(angle)), r*sin(to_radians(angle)))
+            dot(50, tuple(map(float,bright_jzazbz(0.05 + j/100, angle))))
+            update()
+
+def show_cam16ucs_hues():
+    for j in range(1,10):
+        steps = 32
+        if j == 0: steps = 4
+        if j == 1: steps = 8
+        if j == 2: steps = 16
+        for i in range(steps):
+            r = 20 + 50*j
+            angle = 360 * i/steps
+            goto(r*cos(to_radians(angle)), r*sin(to_radians(angle)))
+            dot(50, tuple(map(float,bright_cam16ucs(j*10, angle))))
+            update()
+
+
+#clear()
+#for i in range(1,40,3 if big else 2):
+#    r = 10 * i
+#    points = i*(2 if big else 3)
+#    for j in range(points):
+#        angle = 2*pi * j/points
+#        x = r*cos(angle)
+#        y = r*sin(angle)
+#        goto(x, y)
+#        dot(29 if big else 19, tuple(map(float,bright_jzazbz(0.04 + i/400, angle))))
+#    update()
+
+#x_step = int(round(tan(pi/3) * d/2))+1
+#for x in range(-300,301,x_step):
+#    offset = (x//x_step) % 2 * d//2
+#    for y in range(-300+offset, 301+offset,d+1):
+#        goto(x, y)
+#        angle = atan2(y, x)
+#        r = sqrt(x*x + y*y)
+#        dot(d, tuple(map(float,bright_jzazbz(0.04 + r/4000, angle))))
+#    update()
+
+# NCS
+#colormode(255)
+#for cc in [x for x in ncs if x[0].split('-')[0] == '2050']:
+#    c = lab_to_cam16ucs(*cc[1][-1])
+#    goto(c[1] * 10, c[2] * 10)
+#    dot(20, 'black')
+#    dot(10, cc[1][0])
+
+#colormode(255)
+#for h in hues[::2]:
+#    for cc in ncs[::]:
+#        if cc[0].split('-')[1] == h:
+#            c = lab_to_cam16ucs(*cc[1][-1])
+#            goto(c[1] * 10, c[2] * 10)
+#            #dot(20, 'black')
+#            dot(5, cc[1][0])
+#    update()
+
+# hues='B,B10G,B20G,B30G,B40G,B50G,B60G,B70G,B80G,B90G,G,G10Y,G20Y,G30Y,G40Y,G50Y,G60Y,G70Y,G80Y,G90Y,N,R,R10B,R20B,R30B,R40B,R50B,R60B,R70B,R80B,R90B,Y,Y10R,Y20R,Y30R,Y40R,Y50R,Y60R,Y70R,Y80R,Y90R'.split(',')
+
+def show_kluwer_themes(themes, rows=7, r=50):
+    row = 1
+    for t in themes:
+        for s in t['swatches']:
+            color = '#' + s['hex']
+            dot(r, color)
+            write(color, align='center')
+            forward(r)
+        backward(r*len(t['swatches']))
+        if row < rows:
+            right(90)
+            forward(r)
+            left(90)
+            row = row + 1
+        else:
+            forward(r*6)
+            left(90)
+            forward((rows - 1) * r)
+            right(90)
+            row = 1
+
+
+#import matplotlib.pyplot as plt
+#image = plt.imread('/usr/share/matplotlib/sample_data/grace_hopper.png')
+#image.shape == (600, 512, 3)
+#image_c16 = cam16ucs.from_xyz100(srgb.to_xyz100(srgb.from_srgb1(image.reshape((image.shape[0] * image.shape[1], 3)).T)))
+#pass
+#image_sr = srgb.to_srgb1(srgb.from_xyz100(cam16ucs.to_xyz100(image_c16)))
+#plt.imshow(image_sr.T.reshape(image.shape)); plt.show()
---
+++ ourturtle.py
@@ -0,0 +1,239 @@
+#! /usr/bin/python3
+'''
+Copyright (C) 2005 Aaron Spike, aaron@ekips.org
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+'''
+
+# import traceback
+
+from math import pi, sin, cos, atan2, degrees, radians
+from turtle import TurtleGraphicsError  # Vec2d
+
+from pymunk.vec2d import Vec2d
+
+from named_colors import COLORS
+
+
+class Turtle:
+    _instances = set()
+    _shapes = {
+        "arrow": ("polygon", ((-10, 0), (10, 0), (0, 10))),
+        "turtle":
+        ("polygon",
+         ((0, 16), (-2, 14), (-1, 10), (-4, 7), (-7, 9), (-9, 8), (-6, 5),
+          (-7, 1), (-5, -3), (-8, -6), (-6, -8), (-4, -5), (0, -7),
+          (4, -5), (6, -8), (8, -6), (5, -3), (7, 1), (6, 5), (9, 8),
+          (7, 9), (4, 7), (1, 10), (2, 14))),
+        "circle":
+        ("polygon",
+         ((10, 0), (9.51, 3.09), (8.09, 5.88), (5.88, 8.09), (3.09, 9.51),
+          (0, 10), (-3.09, 9.51), (-5.88, 8.09), (-8.09, 5.88),
+          (-9.51, 3.09), (-10, 0), (-9.51, -3.09), (-8.09, -5.88),
+          (-5.88, -8.09), (-3.09, -9.51), (-0.00, -10.00), (3.09, -9.51),
+          (5.88, -8.09), (8.09, -5.88), (9.51, -3.09))),
+        "square": ("polygon", ((10, -10), (10, 10), (-10, 10), (-10,
+                                                                -10))),
+        "triangle": ("polygon", ((10, -5.77), (0, 11.55), (-10, -5.77))),
+        "classic": ("polygon", ((0, 0), (-5, -9), (0, -7), (5, -9)))
+    }
+
+    def __init__(self):
+        self.reset()
+        self._register()
+
+    def reset(self):
+        self._shown = True
+        self._drawing = True
+        self._pencolor = (.4, .4, 1)  # COLORS['black']
+        self._pensize = 1
+        self._shapesize = 1
+
+        self._position = Vec2d(0, 0)
+        self._lines = []  # [(self._pencolor, self._pensize, [])]
+        self._stamps = []
+        self._orient = Vec2d(1, 0)
+        self._z = 0
+
+        self._new_line()
+
+    def _new_line(self):
+        if self._lines and len(self._lines[-1][2]) < 3:
+            self._lines.pop()
+        self._lines.append((self._pencolor, self._pensize,
+                            list(self._position)))
+
+    def dot(self, r=15, color=None):
+        if not color:
+            color = self._pencolor
+        else:
+            color = self._color(color)
+        self._stamps.append(('Ellipse', self._position, (r, r), color))
+
+    def rect(self, w=25, h=25, color=None):
+        if not color:
+            color = self._pencolor
+        else:
+            color = self._color(color)
+        self._stamps.append(('Rectangle', self._position, (float(w), float(h)),
+                             color))
+
+    def shapesize(self, size):
+        self._shapesize = float(size)
+
+    def forward(self, distance):
+        self._go(distance)
+
+    def backward(self, distance):
+        self._go(-distance)
+
+    def right(self, deg):
+        self._rotate(-deg)
+
+    def left(self, deg):
+        self._rotate(deg)
+
+    def penup(self):
+        self._drawing = False
+        if self._lines and len(self._lines[-1][2]) == 2:
+            self._lines.pop()
+#        self._new = False
+
+    def pendown(self):
+        if not self._drawing:
+            self._new_line()
+            self._drawing = True
+
+    def pentoggle(self):
+        if self._drawing:
+            self.penup()
+        else:
+            self.pendown()
+
+    def pensize(self, width=None):
+        if width:
+            prev_size = self._pensize
+            self._pensize = int(width)
+            if self._drawing is True and self._pensize != prev_size:
+                self._new_line()
+        else:
+            return self._pensize
+
+    def color(self, *args):
+        prev_c = self._pencolor
+        self._pencolor = self._color(*args)
+
+        if self._drawing is True and self._pencolor != prev_c:
+            self._new_line()
+
+    def home(self):
+        self.goto(0, 0)
+        self._orient = Vec2d(1, 0)
+
+#    def clean(self):
+#        self._path = ''
+
+    def clear(self):
+        self.clean()
+        self.home()
+
+    def heading(self):
+        # x, y = self._orient
+        # return round(atan2(y, x)*180.0/pi, 10) % 360.0
+        return self._orient.get_angle_degrees()
+
+    def goto(self, x, y=None):
+        if y is None and isinstance(x, (list, tuple)) and len(x) == 2:
+            self._goto(Vec2d(*map(float, x)))
+        else:
+            self._goto(Vec2d(float(x), float(y)))
+#        if self.__new:
+#            self.__path += "M"+",".join([str(i) for i in self.__pos])
+#            self.__new = False
+#        self.__pos = [x, y]
+#        if self.__draw:
+#            self.__path += "L"+",".join([str(i) for i in self.__pos])
+
+    def position(self):
+        return self._position
+
+    def setheading(self, deg):
+        # self._orient = Vec2d(1, 0)
+        # self._rotate(deg)
+        self._orient.angle_degrees(deg)
+
+    def zlevel(self, z=None):
+        if z:
+            self._z = z
+        else:
+            return self._z
+
+    @staticmethod
+    def turtles():
+        return sorted(Turtle._instances, key=lambda t: t._z)
+
+    @staticmethod
+    def clear_turtles():
+        Turtle._instances = set()
+
+    # fd = forward
+    # bk = backward
+    # rt = right
+    # lt = left
+    # pu = penup
+    # pd = pendown
+    # setpos = goto
+    # position = pos
+
+    #
+    # Private
+    #
+    def _register(self):
+        Turtle._instances.add(self)
+
+    def _unregister(self):
+        if self in Turtle._instances:
+            Turtle._instances.remove(self)
+
+    def _color(self, *color):
+        if len(color) in [3, 4] \
+                and all([isinstance(c, (float, int)) for c in color]):
+            return color
+        elif len(color) == 1 and isinstance(color[0], str):
+            color = color[0]
+            if color.startswith('#'):
+                if len(color) == 7:
+                    return [int(color[i:i + 2], 16) / 255 for i in (1, 3, 5)]
+                elif len(color) == 4:
+                    return [16 * int(h, 16) / 255 for h in color[1:]]
+            elif color in COLORS:
+                return COLORS[color]
+
+        raise TurtleGraphicsError("bad colorstring: %s" % color)
+
+    def _rotate(self, angle):
+        self._orient.rotate_degrees(angle)
+
+    def _go(self, distance):
+        ende = self._position + self._orient * distance
+        self._goto(ende)
+
+    def _goto(self, end):
+        if self._drawing:
+            self._lines[-1][2].extend(list(end))
+        self._position = end
+
+
+# vim: expandtab shiftwidth=4 tabstop=4 softtabstop=4 textwidth=99
