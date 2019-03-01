c775eae126a348bb2633f3bd019ae756b67501c3
user <>
2019-03-01 09:40:05 +0000

    Sokoban: add replay.
    FossilOrigin-Name: 8bb8e52c6989eda45e989d799f3e16b229a4e39b468938397d62e5b326c287e0

--- sokoban/sokoban.py
+++ sokoban/sokoban.py
@@ -21,19 +21,41 @@ class Sokoban:
         self._do_trace = trace
         self.load_level(level, level_set)
 
-    def load_level(self, level=None, level_set=None):
-        self._trace = []
+    def load_level(self, level=None, level_set=None, clear_log=True):
         if not level:
             level = self._level_number
         if not level_set:
             level_set = self._level_set
         self._level = Level(level_set, level)
+        if clear_log or self._level_number != level or self._level_set != level_set:
+            self._log = []
+            self._log_pos = 0
         if self._tiles and (self._level_number != level or self._level_set != level_set):
             self._tiles = None
         self._level_set = level_set
         self._level_number = level
         self._target_found = False
         self._player_pos = self._level.get_player_position()
+
+    def replay(self, pos=None):
+        if pos is None:
+            pos = self._log_pos + 1
+        if pos < self._log_pos:
+            start = 0
+            self.load_level(clear_log=False)
+        else:
+            start = self._log_pos
+        # if pos > len(self._log): return False
+        trace = None
+        self._log_pos = start
+        for dx, dy, steps, trace in self._log[start:pos]:
+            self.move_player(dx, dy, steps, nolog=True)
+        print(2222, start, pos, self._log_pos)
+        return trace
+
+    @property
+    def log(self):
+        return self._log
 
     @property
     def level(self):
@@ -64,23 +86,25 @@ class Sokoban:
                     self._tiles[-1].append(tile)
                     layout.add_widget(tile)
 
-    def move_player(self, dx, dy, steps=1):
-        if self._do_trace:
-            f = inspect.currentframe() 
-            tb = []
-            depth = 0
-            while f is not None:
-                filename = f.f_code.co_filename
-                lineno = f.f_lineno
-                if depth > 4 and filename != '<code-input>':
-                    break
-                # print('depth', depth, filename, lineno)
-                if filename == '<code-input>':
-                    tb.append(lineno)
-                depth += 1
-                f = f.f_back
-            self._trace.append(tb)
-            print('tb', tb)
+    def move_player(self, dx, dy, steps=1, nolog=False):
+        log = self._log
+        if not nolog:
+            trace = []
+            if self._do_trace:
+                f = inspect.currentframe()
+                tb = []
+                depth = 0
+                while f is not None:
+                    filename = f.f_code.co_filename
+                    lineno = f.f_lineno
+                    if depth > 4 and filename != '<code-input>':
+                        break
+                    # print('depth', depth, filename, lineno)
+                    if filename == '<code-input>':
+                        trace.append(lineno)
+                    depth += 1
+                    f = f.f_back
+            log.append((dx, dy, steps, trace))
 
         level = self._level
         target_found = self._target_found
@@ -109,15 +133,19 @@ class Sokoban:
                 elif second_tile == '.':
                     second_tile = '*'
                 else:
+                    if not nolog:
+                        log.pop()
                     raise CannotGo
             else:
+                if not nolog:
+                    log.pop()
                 raise CannotGo
 
-            if target_found == True:
+            if target_found is True:
                 orig_tile = '.'
                 target_found = False
             else:
-                orig_tile = ' '                    
+                orig_tile = ' '
             if next_tile in '.*':
                 target_found = True
             next_tile = '@'
@@ -127,9 +155,13 @@ class Sokoban:
             matrix[y][x] = orig_tile
             matrix[y + dy][x + dx] = next_tile
             matrix[y + 2 * dy][x + 2 * dx] = second_tile
+
             # print('STEP', dx, dy, steps)
             steps -= 1
 
+        self._log_pos += 1
+        # for row in reversed(matrix):
+        #     print(''.join(row))
         # print("Boxes remaining: " + str(len(level.get_boxes())))
         # return len(level.get_boxes()) == 0
         # if len(level.get_boxes()) == 0:
